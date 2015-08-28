f2ad5008490168b21ee6fae2bd09f1deeecc916a
Paul Schweppe <paul@fluid-rock.com>
2015-08-28 15:37:19 +0100

    Created Issue tracking entities

---
+++ src/VersionContol/GitControlBundle/Entity/Issue.php
@@ -0,0 +1,410 @@
+<?php
+
+namespace VersionContol\GitControlBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Issue
+ *
+ * @ORM\Table(name="issue", indexes={@ORM\Index(name="fk_issue_ver_user1_idx", columns={"ver_user_id"}), @ORM\Index(name="fk_issue_project1_idx", columns={"project_id"}), @ORM\Index(name="fk_issue_issue_milestone1_idx", columns={"issue_milestone_id"})})
+ * @ORM\Entity
+ */
+class Issue
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=45, nullable=true)
+     */
+    private $status;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="closed_at", type="datetime", nullable=true)
+     */
+    private $closedAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="github_number", type="integer", nullable=true)
+     */
+    private $githubNumber;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \VersionContol\GitControlBundle\Entity\IssueMilestone
+     *
+     * @ORM\ManyToOne(targetEntity="VersionContol\GitControlBundle\Entity\IssueMilestone")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="issue_milestone_id", referencedColumnName="id")
+     * })
+     */
+    private $issueMilestone;
+
+    /**
+     * @var \VersionContol\GitControlBundle\Entity\Project
+     *
+     * @ORM\ManyToOne(targetEntity="VersionContol\GitControlBundle\Entity\Project")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="project_id", referencedColumnName="id")
+     * })
+     */
+    private $project;
+
+    /**
+     * @var \VersionContol\GitControlBundle\Entity\VerUser
+     *
+     * @ORM\ManyToOne(targetEntity="VersionContol\GitControlBundle\Entity\VerUser")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="ver_user_id", referencedColumnName="id")
+     * })
+     */
+    private $verUser;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\ManyToMany(targetEntity="VersionContol\GitControlBundle\Entity\IssueLabel", inversedBy="issue")
+     * @ORM\JoinTable(name="issue_has_issue_label",
+     *   joinColumns={
+     *     @ORM\JoinColumn(name="issue_id", referencedColumnName="id")
+     *   },
+     *   inverseJoinColumns={
+     *     @ORM\JoinColumn(name="issue_label_id", referencedColumnName="id")
+     *   }
+     * )
+     */
+    private $issueLabel;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->issueLabel = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Issue
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Issue
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return Issue
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set closedAt
+     *
+     * @param \DateTime $closedAt
+     *
+     * @return Issue
+     */
+    public function setClosedAt($closedAt)
+    {
+        $this->closedAt = $closedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get closedAt
+     *
+     * @return \DateTime
+     */
+    public function getClosedAt()
+    {
+        return $this->closedAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Issue
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Issue
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set githubNumber
+     *
+     * @param integer $githubNumber
+     *
+     * @return Issue
+     */
+    public function setGithubNumber($githubNumber)
+    {
+        $this->githubNumber = $githubNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get githubNumber
+     *
+     * @return integer
+     */
+    public function getGithubNumber()
+    {
+        return $this->githubNumber;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set issueMilestone
+     *
+     * @param \VersionContol\GitControlBundle\Entity\IssueMilestone $issueMilestone
+     *
+     * @return Issue
+     */
+    public function setIssueMilestone(\VersionContol\GitControlBundle\Entity\IssueMilestone $issueMilestone = null)
+    {
+        $this->issueMilestone = $issueMilestone;
+
+        return $this;
+    }
+
+    /**
+     * Get issueMilestone
+     *
+     * @return \VersionContol\GitControlBundle\Entity\IssueMilestone
+     */
+    public function getIssueMilestone()
+    {
+        return $this->issueMilestone;
+    }
+
+    /**
+     * Set project
+     *
+     * @param \VersionContol\GitControlBundle\Entity\Project $project
+     *
+     * @return Issue
+     */
+    public function setProject(\VersionContol\GitControlBundle\Entity\Project $project = null)
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    /**
+     * Get project
+     *
+     * @return \VersionContol\GitControlBundle\Entity\Project
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+
+    /**
+     * Set verUser
+     *
+     * @param \VersionContol\GitControlBundle\Entity\VerUser $verUser
+     *
+     * @return Issue
+     */
+    public function setVerUser(\VersionContol\GitControlBundle\Entity\VerUser $verUser = null)
+    {
+        $this->verUser = $verUser;
+
+        return $this;
+    }
+
+    /**
+     * Get verUser
+     *
+     * @return \VersionContol\GitControlBundle\Entity\VerUser
+     */
+    public function getVerUser()
+    {
+        return $this->verUser;
+    }
+
+    /**
+     * Add issueLabel
+     *
+     * @param \VersionContol\GitControlBundle\Entity\IssueLabel $issueLabel
+     *
+     * @return Issue
+     */
+    public function addIssueLabel(\VersionContol\GitControlBundle\Entity\IssueLabel $issueLabel)
+    {
+        $this->issueLabel[] = $issueLabel;
+
+        return $this;
+    }
+
+    /**
+     * Remove issueLabel
+     *
+     * @param \VersionContol\GitControlBundle\Entity\IssueLabel $issueLabel
+     */
+    public function removeIssueLabel(\VersionContol\GitControlBundle\Entity\IssueLabel $issueLabel)
+    {
+        $this->issueLabel->removeElement($issueLabel);
+    }
+
+    /**
+     * Get issueLabel
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getIssueLabel()
+    {
+        return $this->issueLabel;
+    }
+}
---
+++ src/VersionContol/GitControlBundle/Entity/IssueComment.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace VersionContol\GitControlBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * IssueComment
+ *
+ * @ORM\Table(name="issue_comment", indexes={@ORM\Index(name="fk_issue_comment_ver_user1_idx", columns={"ver_user_id"})})
+ * @ORM\Entity
+ */
+class IssueComment
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comment", type="text", nullable=true)
+     */
+    private $comment;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \VersionContol\GitControlBundle\Entity\VerUser
+     *
+     * @ORM\ManyToOne(targetEntity="VersionContol\GitControlBundle\Entity\VerUser")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="ver_user_id", referencedColumnName="id")
+     * })
+     */
+    private $verUser;
+
+
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return IssueComment
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return IssueComment
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return IssueComment
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set verUser
+     *
+     * @param \VersionContol\GitControlBundle\Entity\VerUser $verUser
+     *
+     * @return IssueComment
+     */
+    public function setVerUser(\VersionContol\GitControlBundle\Entity\VerUser $verUser = null)
+    {
+        $this->verUser = $verUser;
+
+        return $this;
+    }
+
+    /**
+     * Get verUser
+     *
+     * @return \VersionContol\GitControlBundle\Entity\VerUser
+     */
+    public function getVerUser()
+    {
+        return $this->verUser;
+    }
+}
