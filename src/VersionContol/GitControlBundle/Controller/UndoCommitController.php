7acf9b81ffbb8a52a1bac2cea218dbaaf262d676
Paul Schweppe <paul@fluid-rock.com>
2015-10-23 16:41:29 +0100

    Added rollback options to git history

---
+++ src/VersionContol/GitControlBundle/Controller/UndoCommitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace VersionContol\GitControlBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use VersionContol\GitControlBundle\Entity\Project;
+use VersionContol\GitControlBundle\Form\ProjectType;
+use VersionContol\GitControlBundle\Utility\GitCommands;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use VersionContol\GitControlBundle\Entity\UserProjects;
+
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+/**
+ * Project controller.
+ *
+ * @Route("/project/undo")
+ */
+class UndoCommitController extends Controller
+{
+
+    /**
+     * Pulls git repository from remote to local.
+     *
+     * @Route("softcommit/{id}", name="undocommit_soft")
+     * @Method("GET")
+     * @Template("VersionContolGitControlBundle:Error:request.html.twig")
+     */
+    public function undoSoftCommitAction($id, Request $request){
+        $em = $this->getDoctrine()->getManager();
+
+        $project= $em->getRepository('VersionContolGitControlBundle:Project')->find($id);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+        
+        $this->checkProjectAuthorization($project,'OPERATOR');
+
+        $gitCommands = $this->get('version_control.git_command')->setProject($project);
+        $response = $gitCommands->undoCommit();
+        $response .= ' If you pushed the last commit to a remote server you will have to pull from remote before it will allow you to push again.';
+        $this->get('session')->getFlashBag()->add('notice', $response);
+        
+        return $this->redirect($this->generateUrl('project_show', array('id' => $project->getId())));
+    }
+    
+    /**
+     * 
+     * @param VersionContol\GitControlBundle\Entity\Project $project
+     * @throws AccessDeniedException
+     */
+    protected function checkProjectAuthorization(\VersionContol\GitControlBundle\Entity\Project $project,$grantType='MASTER'){
+        $authorizationChecker = $this->get('security.authorization_checker');
+
+        // check for edit access
+        if (false === $authorizationChecker->isGranted($grantType, $project)) {
+            throw new AccessDeniedException();
+        }
+    }
+}
+
