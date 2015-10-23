30809a7d7f65f2d4d67136b68b085bc67fa1954e
Paul Schweppe <paul@fluid-rock.com>
2015-10-23 16:52:07 +0100

    Added checkout button. Not finished

--- src/VersionContol/GitControlBundle/Controller/UndoCommitController.php
+++ src/VersionContol/GitControlBundle/Controller/UndoCommitController.php
@@ -24,13 +24,41 @@ class UndoCommitController extends Controller
 {
 
     /**
-     * Pulls git repository from remote to local.
+     * Action to do a soft undo on the last commit. This will 
+     * allow you to fix any messages in the last commit. This 
+     * will not effect any files.
      *
      * @Route("softcommit/{id}", name="undocommit_soft")
      * @Method("GET")
      * @Template("VersionContolGitControlBundle:Error:request.html.twig")
      */
     public function undoSoftCommitAction($id, Request $request){
+        $em = $this->getDoctrine()->getManager();
+
+        $project= $em->getRepository('VersionContolGitControlBundle:Project')->find($id);
+
+        if (!$project) {
+            throw $this->createNotFoundException('Unable to find Project entity.');
+        }
+        
+        $this->checkProjectAuthorization($project,'OPERATOR');
+
+        $gitCommands = $this->get('version_control.git_command')->setProject($project);
+        $response = $gitCommands->undoCommit();
+        $response .= ' If you pushed the last commit to a remote server you will have to pull from remote before it will allow you to push again.';
+        $this->get('session')->getFlashBag()->add('notice', $response);
+        
+        return $this->redirect($this->generateUrl('project_show', array('id' => $project->getId())));
+    }
+    
+    /**
+     * Action to do a hard undo on the last commit. 
+     *
+     * @Route("hardcommit/{id}", name="undocommit_hard")
+     * @Method("GET")
+     * @Template("VersionContolGitControlBundle:Error:request.html.twig")
+     */
+    public function undoHardCommitAction($id, Request $request){
         $em = $this->getDoctrine()->getManager();
 
         $project= $em->getRepository('VersionContolGitControlBundle:Project')->find($id);
