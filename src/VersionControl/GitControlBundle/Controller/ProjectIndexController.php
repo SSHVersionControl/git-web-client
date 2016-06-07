SSHVersionControl/git-web-client

commit 6e5a42cea8c9318e044871b4e2dfe35824ba2d89

Browser refresh now loads correct section Side navigation changes active state.

src/VersionControl/GitControlBundle/Controller/ProjectIndexController.php
@@ -38,13 +38,17 @@ class ProjectIndexController extends BaseProjectController
     /**
      * Lists all Project entities.
      *
-     * @Route("/", name="project")
+     * @Route("/{section}", defaults={"$section" = ""}, name="project")
      * @Method("GET")
      * @Template()
      * @ProjectAccess(grantType="VIEW")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request,$section= "")
     {
+        if($section){
+            $section = urldecode($section);
+        }
+        
         //Get latest from updates from remot branches
         $response = $this->gitCommands->command('branch')->fetchAll();
         
@@ -57,6 +61,7 @@ class ProjectIndexController extends BaseProjectController
         return array_merge($this->viewVariables, array(
             'pushPullCommitCount' => $pushPullCommitCount,
             'statusCount' => $statusCount,
+            'section' => $section,
             ));
     }
     

src/VersionControl/GitControlBundle/Resources/public/js/ajaxifypage.js
@@ -106,7 +106,20 @@ $(function(){
         setUrlHistory(url);
         $element.load( url,function(){
             $element.unmask();
-        });  
+        }); 
+        
+        //Side Navigation state
+        $('.sidebar-menu a').each(function(){
+            $this = $(this);
+            if( url.indexOf(($(this).attr('href'))) >= 0){
+                var $parentUl = $(this).parent('li').addClass('active').parent('ul');
+                if($parentUl.hasClass('treeview-menu')){
+                    $parentUl.closest('li').addClass('active');
+                }
+            }else{
+                $(this).parent('li').removeClass('active');
+            }
+        });
     }
     
     function setUrlHistory(url){
@@ -132,5 +145,8 @@ $(function(){
         }
         firstLoad = true;
     });
+    
+    //Open Default page
+    loadUrl( $contentContainter,defaultPage,'Loading...');
 
 });

src/VersionControl/GitControlBundle/Controller/Base/BaseProjectController.php
@@ -110,7 +110,9 @@ abstract class BaseProjectController extends Controller{
         //Redirect is not ajax
         $request  = $this->getRequest();
         if( $this->ajaxOnly == true && !$request->isXmlHttpRequest()){
-             return $this->generateUrl('project');
+
+             //print_r($request->getRequestUri());
+             return $this->generateUrl('project',array('section'=> urlencode($request->getRequestUri())));
         }
         
         $this->checkProjectAuthorization($this->project,$grantType);

src/VersionControl/GitControlBundle/Controller/ProjectFilesController.php
@@ -37,11 +37,6 @@ class ProjectFilesController extends BaseProjectController{
      */
     protected $gitFilesCommands;
     
-    /**
-     * Allow access by ajax only request
-     * @var boolean 
-     */
-    protected $ajaxOnly = false;
     
     /**
      * Show Git commit diff

src/VersionControl/GitControlBundle/Resources/public/js/app.js
@@ -225,8 +225,7 @@ $(function(){
         //alert('open');
     });
     
-    //Open Default page
-    $('.content-wrapper').load( defaultPage,function(){});
+    
     
     
 });

src/VersionControl/GitControlBundle/Resources/views/ProjectIndex/index.html.twig
@@ -26,7 +26,11 @@
 
  {% block footerJS %}
      <script>
-        var defaultPage = "{{ path('project_commitlist', { 'id': project.id }) }}"; 
+        {% if (section) %}
+            var defaultPage = "{{ section }}";
+        {% else %}
+            var defaultPage = "{{ path('project_commitlist', { 'id': project.id }) }}"; 
+        {% endif %}
 
      </script>
      <script src="{{ asset('bundles/versioncontrolgitcontrol/js/ajaxifypage.js') }}"></script>

commit 63f046be71858e28018a25ef1b6322e2d6c2a3f2

Fix issue with refresh status introduced in previous commit 6e5a42c

src/VersionControl/GitControlBundle/Controller/ProjectIndexController.php
@@ -36,6 +36,33 @@ class ProjectIndexController extends BaseProjectController
     protected $ajaxOnly = false;
     
     /**
+     * Get stats as json object.
+     *
+     * @Route("/status", name="project_status_ajax")
+     * @Method("GET")
+     * @ProjectAccess(grantType="VIEW")
+     */
+    public function statusAction(Request $request)
+    {
+        //Get latest from updates from remot branches
+        $this->gitCommands->command('branch')->fetchAll();
+        
+        $pushPullCommitCount = $this->gitCommands->command('sync')->commitCountWithRemote($this->branchName);
+        
+        $statusCount = $this->gitCommands->command('commit')->countStatus();
+        
+        $response = array(
+            'success' => true,
+            'pushCount' => $pushPullCommitCount['pushCount'],
+            'pullCount' => $pushPullCommitCount['pullCount'],
+            'statusCount' => $statusCount
+        );
+        
+        return new JsonResponse($response);
+        
+    }
+    
+    /**
      * Lists all Project entities.
      *
      * @Route("/{section}", defaults={"$section" = ""}, name="project")
@@ -65,30 +92,5 @@ class ProjectIndexController extends BaseProjectController
             ));
     }
     
-    /**
-     * Get stats as json object.
-     *
-     * @Route("/status", name="project_status_ajax")
-     * @Method("GET")
-     * @ProjectAccess(grantType="VIEW")
-     */
-    public function statusAction(Request $request)
-    {
-        //Get latest from updates from remot branches
-        $this->gitCommands->command('branch')->fetchAll();
-        
-        $pushPullCommitCount = $this->gitCommands->command('sync')->commitCountWithRemote($this->branchName);
-        
-        $statusCount = $this->gitCommands->command('commit')->countStatus();
-        
-        $response = array(
-            'success' => true,
-            'pushCount' => $pushPullCommitCount['pushCount'],
-            'pullCount' => $pushPullCommitCount['pullCount'],
-            'statusCount' => $statusCount
-        );
-        
-        return new JsonResponse($response);
-        
-    }
+   
 }

src/VersionControl/GitControlBundle/Resources/public/js/ajaxifypage.js
@@ -82,10 +82,11 @@ $(function(){
     /* Trigger for side menu refresh status */
     $('#refresh-nav').on('status-refresh',function(){
         //Pace.ignore(function(){
-            $.getJSON( $(this).attr('href'), function( data ) {
-
-                console.log(data);
-
+        
+         $.ajax({
+            url: $(this).attr('href'),
+            dataType: "json",
+            success: function(data) {
                 $.each( data, function( id, val ) {
                     var $label = $('#'+id);
                     if(val == 0){
@@ -95,9 +96,12 @@ $(function(){
                     }
                     $label.html(val);
                 });
-
-            });
-        //});
+            },
+            error: function(e) 
+            {
+                 console.log(e);
+            }
+        });
     });
     
     function loadUrl($element,url,loadingText){
@@ -137,7 +141,7 @@ $(function(){
             Safari emits a popstate event on page load - check if firstLoad is true before animating
             if it's false - the page has just been loaded 
             */
-           console.log(location.pathname);
+           //console.log(location.pathname);
             //var newPageArray = location.pathname.split('/'),
             //this is the url of the page to be loaded 
             //newPage = newPageArray[newPageArray.length - 1].replace('.html', '');